376fc812691d66a9ce2bd40e2dc277243ee91280
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2021-07-06 18:03:45 -0300

    Finish Layout Producer

---
+++ app/src/main/java/com/feiradagua/feiradagua/view/activitys/producer/ProducerUpdateProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.feiradagua.feiradagua.view.activitys.producer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feiradagua.feiradagua.databinding.ActivityProducerUpdateProfileBinding
+
+class ProducerUpdateProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProducerUpdateProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProducerUpdateProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
